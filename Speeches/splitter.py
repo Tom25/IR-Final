f24a387f90d8e35f602bdd81fb02da5c4e513977
Vamsi Chunduru <vchundu1@ugrad6.cs.jhu.edu>
2016-05-12 20:30:59 -0400

    splitter.py done

--- Speeches/splitter.py
+++ Speeches/splitter.py
@@ -1,17 +1,46 @@
 # Transcript Splitter
 
 import sys
+import re
 
-def splitFile(fname):
+def splitFile(fname, cname):
 	f = open(fname, 'r')
 
+	ctext = []
+
+	text = ""
+	while True:
+		line = f.readline()
+		if line.startswith(cname.upper() + ':'):
+			text += line
+			break
+
+	store = True
 	for line in f:
 		speaker = re.match(r'(^[A-Z]+:)',line)
-		if speaker: print speaker
+		if speaker:
+			if text != "": ctext.append(text)
+			text = ""
+			if line.startswith(cname.upper() + ':'):
+				store = True
+			else: store = False
+
+		if store:
+			text += line.strip()
+
+	return ctext
+
+def writeFile(ctext,fname,cname):
+	sname = cname.upper() + '.' + fname[0:len(fname)-4] + '.txt'
+	f = open(sname, 'w')
+	for line in ctext:
+		f.write(line + '\n')
+	f.close()
 
 def main():
-	splitFile(sys.argv[1])
-
-
+	fname = sys.argv[1]
+	cname = sys.argv[2]
+	ctext = splitFile(fname, cname)
+	writeFile(ctext,fname,cname)
 
 main()
